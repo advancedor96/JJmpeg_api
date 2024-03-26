94c1135e9a75741098c735232deec01219a8756b
Ding <advancedor96@gmail.com>
2024-03-26 13:02:01 +0800

    改用 fluent-ffmpeg

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const { spawn } = require('child_process');
-const path = require('path');
-const { exec } = require('child_process');
-// const ffmpeg_s = require('ffmpeg-static');
 
-const ffmpeg = require('ffmpeg');
+const ffmpeg_s = require('ffmpeg-static');
+const ffmpeg = require('fluent-ffmpeg');
 // const ffmpeg_f = require('fluent-ffmpeg');
-const ffmpegPath = path.join(__dirname, 'bin', 'ffmpeg'); 
-const command = `chmod +x ${ffmpegPath}`;
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`执行出错: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-  console.log(`${ffmpegPath} 权限已改变，现在是可执行的。`);
-});
+
+
+ffmpeg.setFfmpegPath(ffmpeg_s);
+
+// 確認執行權限
+// const command = `chmod +x ${ffmpegPath}`;
+// exec(command, (error, stdout, stderr) => {
+//   if (error) {
+//     console.error(`执行出错: ${error}`);
+//     return;
+//   }
+//   console.log(`stdout: ${stdout}`);
+//   console.error(`stderr: ${stderr}`);
+//   console.log(`${ffmpegPath} 权限已改变，现在是可执行的。`);
+// });
 
 
 const app = express();
-console.log(ffmpeg)
 app.use(express.json());
 
 
@@ -36,73 +36,51 @@ app.get('/download', async (req, res) => {
   res.setHeader('Content-Disposition', 'attachment; filename="downloaded_video.mp4"');
 
   // const ffmpegPath = path.join(__dirname, 'bin', 'ffmpeg'); 
+  ffmpeg(m3u8Url)
+    .outputOptions([
+      '-c copy', // 使用相同的视频和音频编码进行复制
+      '-f mp4', // 输出格式为MP4
+      '-movflags frag_keyframe+empty_moov', // 使输出格式适合流式传输
+      '-bsf:a aac_adtstoasc'
+    ])
+    .on('start', (cmd)=>{
+      console.log('開始下載:',cmd);
+    })
+    .on('end', function() {
+      console.log('下載完成啦！FFinished.');
+    })
+    .on('error', function(err) {
+      console.log('處理發生錯誤QQ: ' + err.message);
+      res.status(500).send('處理發生錯誤QQ');
+    })
+    .on('progress', function(progress) {
+      console.log('進度:',progress.timemark);
+    })
+    .pipe(res, { end: true }); // 将输出直接发送到响应对象
 
-
-  const ffmpegProcess = spawn(ffmpegPath, [
-    '-i', m3u8Url,  // 输入视频URL
-    '-c', 'copy',    // 使用相同的视频和音频编码进行复制
-    '-f', 'mp4',     // 输出格式为MP4
-    '-movflags', 'frag_keyframe+empty_moov', // 使输出格式适合流式传输
-    '-bsf:a', 'aac_adtstoasc',
-    'pipe:1'         // 输出到stdout
-  ]);
-  ffmpegProcess.stdout.pipe(res);
-
-  // 处理错误
-  ffmpegProcess.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-  ffmpegProcess.on('close', (code) => {
-    if (code !== 0) {
-      console.log(`FFmpeg process exited with code ${code}`);
-    }
-    res.end();
-  });
 });
 
-  
-  // try {
-  //   const outputPath = `JJ_output_${Date.now()}.mp4`;
-  //   const stream = ffmpeg(m3u8Url)
-  //   .outputOptions(['-c copy'])
-  //   .output(outputPath)
-
-  //   console.log('3333');
-
-  //   res.setHeader('Content-Type', 'video/mp4');
-  //   res.setHeader('Content-Disposition', `attachment; filename="${outputPath}"`);
-
-  //   stream.on('start', (commandLine) => {
-  //     console.log('start!!.....FFmpeg command:', commandLine);
-  //   });
-
-  //   stream.on('data', (chunk) => {
-  //     console.log('on data ing...');
-      
-  //     res.write(chunk);
-  //   });
-
-  //   stream.on('end', () => {
-  //     res.end();
-  //   });
-
-  //   stream.on('error', (err) => {
-  //     console.log('111, error');
-  //     console.error(err);
-  //     res.status(500).send('主程式 Error downloading stream');
-  //   });
-  // } catch (err) {
-  //   console.log('222');
-  //   console.error(err);
-  //   res.status(500).send('主程式 Error downloading stream');
-  // }
-// });
-
-function downloadStream(url, outputPath) {
-  console.log('這裡，ffmpeg',ffmpeg);
-
-  return  
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+  // 原本的
+  // const ffmpegProcess = spawn(ffmpegPath, [
+  //   '-i', m3u8Url,  // 输入视频URL
+  //   '-c', 'copy',    // 使用相同的视频和音频编码进行复制
+  //   '-f', 'mp4',     // 输出格式为MP4
+  //   '-movflags', 'frag_keyframe+empty_moov', // 使输出格式适合流式传输
+  //   '-bsf:a', 'aac_adtstoasc',
+  //   'pipe:1'         // 输出到stdout
+  // ]);
+  // ffmpegProcess.stdout.pipe(res);
+
+  // ffmpegProcess.stderr.on('data', (data) => {
+  //   console.error(`stderr: ${data}`);
+  // });
+  // ffmpegProcess.on('close', (code) => {
+  //   if (code !== 0) {
+  //     console.log(`FFmpeg process exited with code ${code}`);
+  //   }
+  //   res.end();
+  // });
